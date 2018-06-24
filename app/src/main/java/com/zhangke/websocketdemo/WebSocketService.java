082acc18e6e3dda8762f95bba8453eff135b37d1
ZhangKe <zhangke_711@163.com>
2018-06-25 00:11:23 +0800

    删除过旧版本代码

--- app/src/main/java/com/zhangke/websocketdemo/WebSocketService.java
+++ app/src/main/java/com/zhangke/websocketdemo/WebSocketService.java
@@ -1,44 +1,38 @@
 package com.zhangke.websocketdemo;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.TypeReference;
-import com.zhangke.WebSocket.AbsBaseWebSocketService;
-import com.zhangke.WebSocket.CommonResponse;
-import com.zhangke.WebSocket.event.WebSocketSendDataErrorEvent;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
 
-import org.greenrobot.eventbus.EventBus;
+import com.zhangke.websocket.BaseWebSocketService;
 
 /**
  * Created by zk721 on 2018/1/28.
  */
 
-public class WebSocketService extends AbsBaseWebSocketService {
+public class WebSocketService extends BaseWebSocketService {
 
     @Override
     protected String getConnectUrl() {
         return "服务器对应的url";
     }
 
-    @Override
-    protected void dispatchResponse(String textResponse) {
-        //处理数据
-        try {
-            CommonResponse<String> response = JSON.parseObject(textResponse, new TypeReference<CommonResponse<String>>() {
-            });
-            if (response == null) {
-                EventBus.getDefault().post(new WebSocketSendDataErrorEvent("", textResponse, "响应数据为空"));
-                return;
-            }
-            //此处可根据服务器接口文档进行调整，判断 code 值是否合法，如下：
-//            if (response.getCode() >= 1000 && response.getCode() < 2000) {
-//                EventBus.getDefault().post(response);
-//            }else{
-//                EventBus.getDefault().post(new WebSocketSendDataErrorEvent(response.getCommand().getPath(), textResponse, response.getMsg()));
-//            }
-            EventBus.getDefault().post(response);
-        }catch(Exception e){
-            //一般由于 JSON 解析时出现异常
-            EventBus.getDefault().post(new WebSocketSendDataErrorEvent("", textResponse, "数据异常:" + e.getMessage()));
+    private WebSocketService.ServiceBinder serviceBinder = new WebSocketService.ServiceBinder();
+
+    public class ServiceBinder extends Binder {
+        public WebSocketService getService() {
+            return WebSocketService.this;
         }
     }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        if (serviceBinder == null) {
+            serviceBinder = new WebSocketService.ServiceBinder();
+        }
+        return serviceBinder;
+    }
+
 }
