fe64b1ee60332553af5881bf52f232c7cc3f96e2
张可_711 <zhangke@zlddata.cn>
2019-03-25 21:27:30 +0800

    添加文本工具类

---
+++ websocketlib/src/main/java/com/zhangke/websocket/util/TextUtil.java
@@ -0,0 +1,16 @@
+package com.zhangke.websocket.util;
+
+/**
+ * 字符串处理相关工具
+ * <p>
+ * Created by ZhangKe on 2019/3/25.
+ */
+public class TextUtil {
+
+    /**
+     * 判断字符串是否为空
+     */
+    public static boolean isEmpty(String text) {
+        return text == null || text.isEmpty();
+    }
+}
