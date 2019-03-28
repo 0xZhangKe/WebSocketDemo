6330cd2d9800cafbaec1b327d0031239fc394c89
张可_711 <zhangke@zlddata.cn>
2019-03-28 16:39:46 +0800

    删除无用的引用

--- websocketlib/src/main/java/com/zhangke/websocket/dispatcher/ResponseDelivery.java
+++ websocketlib/src/main/java/com/zhangke/websocket/dispatcher/ResponseDelivery.java
@@ -1,16 +1,6 @@
 package com.zhangke.websocket.dispatcher;
 
 import com.zhangke.websocket.SocketListener;
-import com.zhangke.websocket.response.ErrorResponse;
-import com.zhangke.websocket.response.Response;
-
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-
-import static com.zhangke.websocket.util.ThreadUtil.checkMainThread;
-import static com.zhangke.websocket.util.ThreadUtil.runOnMainThread;
 
 /**
  * 消息发射器接口
@@ -22,6 +12,8 @@ public interface ResponseDelivery extends SocketListener {
     void addListener(SocketListener listener);
 
     void removeListener(SocketListener listener);
+
+    void clear();
 
     boolean isEmpty();
 }
