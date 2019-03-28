bf231ddfadd4d23b9252c58797492fc5b619bd9e
张可_711 <zhangke@zlddata.cn>
2019-03-28 16:51:57 +0800

    添加重连管理类的接口

--- websocketlib/src/main/java/com/zhangke/websocket/ReconnectManager.java
+++ websocketlib/src/main/java/com/zhangke/websocket/ReconnectManager.java
@@ -1,96 +1,50 @@
 package com.zhangke.websocket;
 
-import android.os.Handler;
-import android.util.Log;
-
-import com.zhangke.websocket.response.MessageType;
-
-import org.java_websocket.client.WebSocketClient;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 /**
- * 负责 WebSocket 重连
+ * 重连接口
  * <p>
- * Created by ZhangKe on 2018/6/24.
+ * Created by ZhangKe on 2019/3/27.
  */
-public class ReconnectManager {
-
-    private static final String TAG = "WebSocketLib";
-
-    private WebSocketWrapper mWebSocketThread;
+public interface ReconnectManager {
 
     /**
      * 是否正在重连
      */
-    private volatile boolean retrying;
-    private volatile boolean destroyed;
-
-    private final ExecutorService singleThreadPool = Executors.newSingleThreadExecutor();
-
-    ReconnectManager(WebSocketWrapper mWebSocketThread) {
-        this.mWebSocketThread = mWebSocketThread;
-        retrying = false;
-        destroyed = false;
-    }
-
-    /**
-     * 开始重新连接，连接方式为每个500ms连接一次，持续十五次。
-     */
-    synchronized void performReconnect() {
-        if (retrying) {
-            Log.i(TAG, "正在重连，请勿重复调用。");
-        } else {
-            retry();
-        }
-    }
+    boolean reconnecting();
 
     /**
      * 开始重连
      */
-    private synchronized void retry() {
-        if (!retrying) {
-            retrying = true;
-            synchronized (singleThreadPool) {
-                singleThreadPool.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        retrying = true;
-                        for (int i = 0; i < 20; i++) {
-                            if (destroyed) {
-                                retrying = false;
-                                return;
-                            }
-                            Handler handler = mWebSocketThread.getHandler();
-                            WebSocketClient websocket = mWebSocketThread.getSocket();
-                            if (handler != null && websocket != null) {
-                                if (mWebSocketThread.getConnectState() == 2) {
-                                    break;
-                                } else if (mWebSocketThread.getConnectState() == 1) {
-                                    continue;
-                                } else {
-                                    handler.sendEmptyMessage(MessageType.CONNECT);
-                                }
-                            } else {
-                                break;
-                            }
-                        }
-                        retrying = false;
-                    }
-                });
-            }
-        }
-    }
+    void startReconnect();
 
     /**
-     * 销毁资源，并停止重连
+     * 停止重连
      */
-    void destroy() {
-        destroyed = true;
-        if (singleThreadPool != null) {
-            singleThreadPool.shutdownNow();
-        }
-        mWebSocketThread = null;
+    void stopReconnect();
+
+    /**
+     * 连接成功
+     */
+    void onConnected();
+
+    /**
+     * 连接失败
+     *
+     * @param th 失败原因
+     */
+    void onConnectError(Throwable th);
+
+    /**
+     * 销毁资源
+     */
+    void destroy();
+
+    /**
+     * 连接成功或失败事件
+     */
+    interface OnConnectListener {
+        void onConnected();
+
+        void onDisconnect();
     }
 }
