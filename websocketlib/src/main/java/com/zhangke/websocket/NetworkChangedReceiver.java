8e13fa60ec60935b8cabaf7d77ba9f9282a01221
张可_711 <zhangke@zlddata.cn>
2019-04-01 17:36:35 +0800

    修改重连判断及调用方式

--- websocketlib/src/main/java/com/zhangke/websocket/NetworkChangedReceiver.java
+++ websocketlib/src/main/java/com/zhangke/websocket/NetworkChangedReceiver.java
@@ -7,7 +7,10 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.annotation.RequiresPermission;
-import android.util.Log;
+
+import com.zhangke.websocket.util.LogUtil;
+
+import java.util.Map;
 
 /**
  * 监听网络变化广播，网络变化时自动重连
@@ -17,13 +20,7 @@ public class NetworkChangedReceiver extends BroadcastReceiver {
 
     private static final String LOGTAG = "NetworkChangedReceiver";
 
-    private WebSocketManager webSocketManager;
-
     public NetworkChangedReceiver() {
-    }
-
-    public NetworkChangedReceiver(WebSocketManager webSocketManager) {
-        this.webSocketManager = webSocketManager;
     }
 
     @RequiresPermission(Manifest.permission.ACCESS_NETWORK_STATE)
@@ -37,15 +34,26 @@ public class NetworkChangedReceiver extends BroadcastReceiver {
             if (activeNetwork != null) {
                 if (activeNetwork.isConnected()) {
                     if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI) {
-                        Log.i(LOGTAG, "网络连接发生变化，当前WiFi连接可用，正在尝试重连。");
+                        LogUtil.i(LOGTAG, "网络连接发生变化，当前WiFi连接可用，正在尝试重连。");
                     } else if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE) {
-                        Log.i(LOGTAG, "网络连接发生变化，当前移动连接可用，正在尝试重连。");
+                        LogUtil.i(LOGTAG, "网络连接发生变化，当前移动连接可用，正在尝试重连。");
                     }
-                    if (webSocketManager != null) {
-                        webSocketManager.reconnect();
+                    if (WebSocketHandler.getDefault() != null) {
+                        if (WebSocketHandler.getDefault().getSetting().reconnectWithNetworkChanged()) {
+                            WebSocketHandler.getDefault().reconnect();
+                        }
+                    }
+                    if (!WebSocketHandler.getAllWebSocket().isEmpty()) {
+                        Map<String, WebSocketManager> webSocketManagerMap = WebSocketHandler.getAllWebSocket();
+                        for (String key : webSocketManagerMap.keySet()) {
+                            WebSocketManager item = webSocketManagerMap.get(key);
+                            if (item != null && item.getSetting().reconnectWithNetworkChanged()) {
+                                item.reconnect();
+                            }
+                        }
                     }
                 } else {
-                    Log.i(LOGTAG, "当前没有可用网络");
+                    LogUtil.i(LOGTAG, "当前没有可用网络");
                 }
             }
         }
